7cbfa016c791e292a03e509056b42f206bc3299b
Shalini-Ashokan <shalini.ashokan@syncfusion.com>
2022-10-11 18:11:07 +0530

    Vertical gauge sample

---
+++ VerticalGauge/VerticalGauge/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace VerticalGauge;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ VerticalGauge/VerticalGauge/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VerticalGauge;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
